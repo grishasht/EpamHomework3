5175cc42f50592b155a15e07425185facf72e3c0
Heorhii <grishasht@bigmir.net>
2019-04-11 23:56:53 +0300

    2-nd task was added

---
+++ src/main/java/task1/CreateTree.java
@@ -0,0 +1,20 @@
+package task1;
+
+import java.util.Scanner;
+
+public class CreateTree{
+    private static Integer[] operands = Parser.createExpression();
+
+    public static Expression calculateExpression(){
+        return new Add(
+                     new Multiply(
+                            new Add(new Single(operands[0]), new Single(operands[1])),
+                            new Single(operands[2])
+                    ),
+                    new Multiply(
+                            new Add(new Single(operands[4]), new Single(operands[5])),
+                            new Single(operands[3])
+                    )
+                );
+    }
+}
---
+++ src/main/java/task1/Multiply.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class Multiply extends Data implements Expression{
+    public Multiply(Expression operand1, Expression operand2) {
+        super(operand1, operand2);
+        expression();
+    }
+
+    @Override
+    public Integer expression() {
+        return operand1.expression() * operand2.expression();
+    }
+}
