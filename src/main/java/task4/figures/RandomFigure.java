d1c3a5f93a6e62bb1eca183d6abb1535b6b0de99
Heorhii <grishasht@bigmir.net>
2019-04-13 19:29:06 +0300

    5-th task added

---
+++ src/main/java/task4/figures/RandomFigure.java
@@ -0,0 +1,12 @@
+package task4.figures;
+
+public class RandomFigure extends FigureData implements Figure{
+    public RandomFigure(Integer rectNum, String color) {
+        super(rectNum, color);
+    }
+
+    @Override
+    public String putFigure() {
+        return "Random figure putted. Its size: " + rectNum + "\tIts color:" + color;
+    }
+}
