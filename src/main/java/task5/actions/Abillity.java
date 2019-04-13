b33c8709c6f51625d4637fc032ee67402d5a7f88
Heorhii <grishasht@bigmir.net>
2019-04-13 20:49:38 +0300

    6-th task added

---
+++ src/main/java/task5/actions/Abillity.java
@@ -0,0 +1,5 @@
+package task5.actions;
+
+public interface Abillity {
+    String useAbillity();
+}
---
+++ src/main/java/task5/actions/Walk.java
@@ -0,0 +1,8 @@
+package task5.actions;
+
+public class Walk implements Abillity{
+    @Override
+    public String useAbillity() {
+        return "walk";
+    }
+}
